cb5806759f39187e34e5cb27729510cadb982684
wesley_wisch <wesley_wisch@hotmail.com>
2023-04-03 17:24:19 -0300

    feat: Doing the tabbed browsing part and starting to style the home screen.

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>dsadad</Text>
-    </View>
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
-    height: 400,
-  },
-  title: {
-    fontSize: 24,
-    marginTop: 100
-  }
-})
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,69 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, SafeAreaView, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+import { Logo } from '../../components/Logo';
 
 export function Home() {
+  const [inputValue, setInputValue] = useState('')
+
+  function handleSearch() {
+    console.log(inputValue)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Página home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Digite o nome da comida..."
+          style={styles.input}
+          value={inputValue}
+          onChangeText={text => setInputValue(text)}
+        />
+
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
-  }
+    flex: 1,
+    backgroundColor: '#f3f9ff',
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#fff",
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#ececec',
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input: {
+    width: "90%",
+    maxWidth: '90%',
+    height: 54,
+  },
 })
