8de9505dd4fc605601e88c23be878d7e4e269121
wesley_wisch <wesley_wisch@hotmail.com>
2023-04-04 18:10:56 -0300

    feat: Creating the component that will display the foods.

--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -1,9 +1,58 @@
-import { View, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, Text, StyleSheet, Image, View } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient'
 
 export function FoodList({ data }) {
   return (
-    <View>
-      <Text>{data.name}</Text>
-    </View>
+    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+      <Image
+        source={{ uri: data.cover }}
+        style={styles.cover}
+      />
+
+      <View style={styles.info}>
+        <Text style={styles.name}>{data.name}</Text>
+        <Text style={styles.description}>{data.total_ingredients} ingredientes | {data.time} min</Text>
+      </View>
+
+      <LinearGradient
+        style={styles.gradient}
+        colors={['transparent', 'rgba(0,0,0,0.70)', 'rgba(0,0,0,0.95)']}
+      />
+    </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 14,
+  },
+  cover: {
+    width: '100%',
+    height: 200,
+    borderRadius: 14,
+  },
+  info: {
+    position: 'absolute',
+    bottom: 14,
+    left: 14,
+    zIndex: 3,
+  },
+  name: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: "bold",
+  },
+  description: {
+    color: '#fff',
+  },
+  gradient: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: '55%',
+    borderRadius: 14,
+    zIndex: 1,
+    backgroundColor: 'transparent',
+  },
+})
