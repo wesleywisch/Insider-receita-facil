90f33ceee481e1cb75e13dbadf2618b25896653b
wesley_wisch <wesley_wisch@hotmail.com>
2023-04-05 15:27:05 -0300

    feat: Performing the details page and starting to make the ingredients component.

---
+++ src/components/Ingredients/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+
+export function Ingredients() {
+  return (
+    <View>
+      <Text>Massa de pizza</Text>
+    </View>
+  )
+}
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,7 +1,9 @@
 import { useLayoutEffect } from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { ScrollView, Text, StyleSheet, Pressable, Image, View } from 'react-native';
 import { useRoute, useNavigation } from '@react-navigation/native'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, AntDesign, Feather } from '@expo/vector-icons'
+
+import { Ingredients } from '../../components/Ingredients'
 
 export function Detail() {
   const route = useRoute()
@@ -19,15 +21,68 @@ export function Detail() {
   }, [route.params?.data, navigate])
 
   return (
-    <View style={styles.container}>
-      <Text>Página Detail</Text>
-      <Text>{route.params?.data.name}</Text>
-    </View>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <Pressable>
+        <View style={styles.playIcon}>
+          <AntDesign name="playcircleo" size={48} color="#fafafa" />
+        </View>
+        <Image
+          source={{ uri: route.params?.data.cover }}
+          style={styles.cover}
+        />
+      </Pressable>
+
+      <View style={styles.headerDetails}>
+        <View>
+          <Text style={styles.title}>{route.params?.data.name}</Text>
+          <Text style={styles.ingredientsText}>Ingredientes: ({route.params?.data.total_ingredients})</Text>
+        </View>
+
+        <Pressable>
+          <Feather name="share-2" size={24} color="#121212" />
+        </Pressable>
+      </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'blue',
-  }
+    backgroundColor: '#f3f9ff',
+    paddingTop: 14,
+    paddingEnd: 14,
+    paddingStart: 14,
+  },
+  cover: {
+    width: '100%',
+    height: 200,
+    borderRadius: 14,
+  },
+  playIcon: {
+    position: 'absolute',
+    zIndex: 3,
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    marginTop: 14,
+    fontWeight: "bold",
+    color: "#000",
+    marginBottom: 4,
+  },
+  ingredientsText: {
+    marginBottom: 14,
+    fontSize: 16,
+  },
+  headerDetails: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 14,
+  },
 })
